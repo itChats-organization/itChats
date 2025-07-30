e3d3d89cb8d35733f84eef3ffb5b91e60dfabab3
basi743 <basicodes743@gmail.com>
2025-07-30 15:59:23 +0200

    Modified firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,12 +1,15 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCOkGvxcZYR5_99Aimwzi8D2Wm2MlU_bO4",
+  authDomain: "itchats-1d993.firebaseapp.com",
+  projectId: "itchats-1d993",
+  storageBucket: "itchats-1d993.firebasestorage.app",
+  messagingSenderId: "905044653718",
+  appId: "1:905044653718:web:22e5ee37f313e81ac2b4b7",
+  measurementId: "G-6Q1YRJ5Y9T"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
